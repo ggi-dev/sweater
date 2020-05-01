4be4f56d3805d3f26b8761f0c269890fb0d89005
ggi-dev <ggi.mail.for.dev@gmail.com>
2020-05-01 22:23:53 +0600

    reCaptcha, rest client, rememberMe

--- src/main/java/com/example/sweater/controller/RegistrationController.java
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -1,8 +1,10 @@
 package com.example.sweater.controller;
 
 import com.example.sweater.domain.User;
+import com.example.sweater.domain.dto.CaptchaResponseDto;
 import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -11,14 +13,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class RegistrationController {
+    private final static String CAPTCHA_URL ="https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${recaptcha.secret}")
+    private String secret;
 
     @GetMapping("/registration")
     public String registration() {
@@ -28,10 +41,17 @@ public class RegistrationController {
     @PostMapping("/registration")
     public String addUser(
             @RequestParam("password2") String passwordConfirm,
+            @RequestParam("g-recaptcha-response") String captchaResponse,
             @Valid User user,
             BindingResult bindingResult,
             Model model
     ) {
+        String url = String.format(CAPTCHA_URL, secret, captchaResponse);
+        CaptchaResponseDto response = restTemplate.postForObject(url, Collections.emptyList(), CaptchaResponseDto.class);
+
+        if (!response.isSuccess()) {
+            model.addAttribute("captchaError", "Fill captcha");
+        }
         boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
         if (isConfirmEmpty) {
             model.addAttribute("password2Error", "Password confirmation cannot empty");
@@ -40,7 +60,10 @@ public class RegistrationController {
         if (user.getPassword() != null && !user.getPassword().equals(passwordConfirm)) {
             model.addAttribute("passwordError", "Passwords are different!");
         }
-        if (isConfirmEmpty || bindingResult.hasErrors()) {
+        if (isConfirmEmpty ||
+                bindingResult.hasErrors() ||
+                !response.isSuccess() ||
+                (user.getPassword() != null && !user.getPassword().equals(passwordConfirm))) {
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
             model.mergeAttributes(errors);
             return "registration";
--- src/main/resources/templates/parts/login.ftl
+++ src/main/resources/templates/parts/login.ftl
@@ -41,20 +41,28 @@
                     </#if>
                 </div>
             </div>
-        <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Email:</label>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Email:</label>
+                <div class="col-sm-6">
+                    <input type="email" name="email"
+                           value="<#if user??>${user.email}</#if>"
+                           class="form-control ${(emailError??)?string('is-invalid', '')}"
+                           placeholder="some@some.com"/>
+                    <#if emailError??>
+                        <div class="invalid-feedback">
+                            ${emailError}
+                        </div>
+                    </#if>
+                </div>
+            </div>
             <div class="col-sm-6">
-                <input type="email" name="email"
-                       value="<#if user??>${user.email}</#if>"
-                       class="form-control ${(emailError??)?string('is-invalid', '')}"
-                       placeholder="some@some.com"/>
-                <#if emailError??>
-                    <div class="invalid-feedback">
-                        ${emailError}
+                <div class="g-recaptcha" data-sitekey="6LfUBfEUAAAAAKEk5NcIhvrsiqSuBwxHRa2lTOdX"></div>
+                <#if captchaError??>
+                    <div class="alert alert-danger" role="alert">
+                        ${captchaError}
                     </div>
                 </#if>
             </div>
-        </div>
         </#if>
         <input type="hidden" name="_csrf" value="${_csrf.token}"/>
         <#if !isRegisterForm><a href="/registration">Registration</a></#if>
